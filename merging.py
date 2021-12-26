e65bfc89ba55d0eac516740c36806f068057358c
Kartik <karkerakartik27@gmail.com>
2021-12-26 11:59:03 +0530

    Added Merge two sorted linked lists

---
+++ merging.py
@@ -0,0 +1,26 @@
+class Solution(object):
+	def mergeTwoLists(self, list1, list2):
+            if not list1 and not list2:
+                return []
+            if not list1:
+                return list2
+            if not list2:
+                return list1
+
+            result = ListNode()
+            head = result
+            while list1 and list2:
+                if list1.val <= list2.val:
+                    head.next = list1
+                    list1 = list1.next
+                else:
+                    head.next = list2
+                    list2 = list2.next
+                head = head.next
+
+            if list1:
+                head.next = list1
+            elif list2:
+                head.next = list2
+
+            return result.next
