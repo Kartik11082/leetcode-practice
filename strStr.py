c53a8216f522b84b25a311fd03e51bbacc639dd4
Kartik <karkerakartik27@gmail.com>
2022-01-05 17:25:20 +0530

    Added Leetcode strStr

---
+++ strStr.py
@@ -0,0 +1,26 @@
+# Time limit exceeded in Leetcode
+# class Solution:
+#     def strStr(self, haystack: str, needle: str) -> int:
+#         if needle == "":
+#             return 0
+# 
+#         for i in range(len(haystack) + 1 - len(needle)):
+#             for j in range(len(needle)):
+#                 if haystack[i + j] != needle[j]:
+#                     break
+#                 if j == len(needle) - 1:
+#                     return i
+# 
+#         return -1
+
+
+
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if needle == "":
+            return 0
+
+        for i in range(len(haystack) + 1 - len(needle)):
+            if haystack[i: i + len(needle)] == needle:
+                return i
+        return -1
