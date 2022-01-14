99ff74c3253c015c92e0e3b28757178e6f36953f
Kartik <karkerakartik27@gmail.com>
2022-01-14 22:44:16 +0530

    Added Merge intervals ([tip: Draw the intervals in a number line] check end with lastEnd)

---
+++ mergeIntervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        class Solution:
+            def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+                intervals = sorted(intervals, key=lambda x:x[0])
+                output = [intervals[0]]
+                for start, end in intervals[1:]:
+                    lastEnd = output[-1][1]
+                    if start <= lastEnd:
+                        output[-1][1] = max(lastEnd, end)
+                    else:
+                        output.append([start, end])
+                return output
