1750cd5e2d4d7fc23052aa0642bc1920fe99a8ee
Kartik <karkerakartik27@gmail.com>
2022-02-02 21:43:24 +0530

    Updated Single Number with Bit Manipulation Solution (XOR of all number in nums is the result as all twin digits cancel out)

--- singleNumber.py
+++ singleNumber.py
@@ -1,3 +1,4 @@
+# Using hashmap
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         hashMap = dict()
@@ -13,3 +14,14 @@ class Solution:
                 return key
 
         return -1
+
+# Using Bit Manipulation
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xorNum = 0
+
+        for n in nums:
+            xorNum ^= n
+
+        return xorNum
+
