28e6bf9ea56ce2ad2c910ca68198609b2d76189b
Kartik <karkerakartik27@gmail.com>
2022-02-02 22:05:20 +0530

    Updated Majority element with Boyer-Moore algorithm

--- majorityElement.py
+++ majorityElement.py
@@ -12,3 +12,42 @@ class Solution:
                 return key
 
         return -1
+
+# Python implementation for the above approach
+
+# Function to find majority element
+def findMajority(arr, n):
+	candidate = -1
+	votes = 0
+	
+	# Finding majority candidate
+	for i in range (n):
+		if (votes == 0):
+			candidate = arr[i]
+			votes = 1
+		else:
+			if (arr[i] == candidate):
+				votes += 1
+			else:
+				votes -= 1
+	count = 0
+	
+	# Checking if majority candidate occurs more than n/2
+	# times
+	for i in range (n):
+		if (arr[i] == candidate):
+			count += 1
+			
+	if (count > n // 2):
+		return candidate
+	else:
+		return -1
+
+# Driver Code
+
+arr = [ 2, 2, 2, 1, 1, 1, 1]
+n = len(arr)
+majority = findMajority(arr, n)
+print(" The majority element is :" ,majority)
+	
+# This code is contributed by shivanisinghss2110
