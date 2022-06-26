06ff930212f932fdf2f3d5d8e0b033b01436b7d1
Kartik Karkera <49190983+Kartik11082@users.noreply.github.com>
2022-06-26 21:23:27 +0530

    Time: 66 ms (7.82%), Space: 13.9 MB (10.63%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -5,28 +5,28 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:        
-        length = self.getLength(head)
-        if length % 2 == 0:
-            mid = (length // 2)
-        else:
-            mid = (length // 2)
+#         length = self.getLength(head)
+#         if length % 2 == 0:
+#             mid = (length // 2)
+#         else:
+#             mid = (length // 2)
         
-        curr = head
-        for i in range(mid):
-            curr = curr.next
+#         curr = head
+#         for i in range(mid):
+#             curr = curr.next
         
-        return curr
+#         return curr
         
         
-    def getLength(self, curr):
-        count = 0
-        while curr != None:
-            count += 1
-            curr = curr.next
-        return count
+#     def getLength(self, curr):
+#         count = 0
+#         while curr != None:
+#             count += 1
+#             curr = curr.next
+#         return count
         
-        # p1 = p2 = head
-        # while p2 and p2.next:
-        #     p1 = p1.next
-        #     p2 = p2.next.next
-        # return p1
+        p1 = p2 = head
+        while p2 and p2.next:
+            p1 = p1.next
+            p2 = p2.next.next
+        return p1
