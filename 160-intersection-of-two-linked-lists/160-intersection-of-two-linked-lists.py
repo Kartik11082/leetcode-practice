3d3caaa16fd5f42870eda5f77d038058c7a2c2b9
Kartik Karkera <49190983+Kartik11082@users.noreply.github.com>
2022-06-27 18:55:54 +0530

    Time: 361 ms (5.01%), Space: 29.5 MB (69.72%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.py
@@ -6,34 +6,42 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        pA, pB = headA, headB
-        lA, lB = self.getLength(headA), self.getLength(headB)
-        diff = abs(lA - lB)
+        l1, l2 = headA, headB
+        while l1 != l2:
+            l1 = l1.next if l1 else headB
+            l2 = l2.next if l2 else headA
+        return l1
+            
+    
+    
+#         pA, pB = headA, headB
+#         lA, lB = self.getLength(headA), self.getLength(headB)
+#         diff = abs(lA - lB)
         
-        if lA == 1 and lB == 1:
-            if pA.val == pB.val:
-                return pA
+#         if lA == 1 and lB == 1:
+#             if pA.val == pB.val:
+#                 return pA
 
-        if lB > lA:
-            while pB.next != None and diff:
-                pB = pB.next
-                diff -= 1
-        elif lA > lB:
-            while pA.next != None and diff:
-                pA = pA.next
-                diff -= 1
+#         if lB > lA:
+#             while pB.next != None and diff:
+#                 pB = pB.next
+#                 diff -= 1
+#         elif lA > lB:
+#             while pA.next != None and diff:
+#                 pA = pA.next
+#                 diff -= 1
         
-        while pA and pB:
-            if pA == pB:
-                return pA
-            pA = pA.next
-            pB = pB.next
-        return None
+#         while pA and pB:
+#             if pA == pB:
+#                 return pA
+#             pA = pA.next
+#             pB = pB.next
+#         return None
             
 
-    def getLength(self, node):
-        count = 0
-        while node:
-            count += 1
-            node = node.next
-        return count
+#     def getLength(self, node):
+#         count = 0
+#         while node:
+#             count += 1
+#             node = node.next
+#         return count
