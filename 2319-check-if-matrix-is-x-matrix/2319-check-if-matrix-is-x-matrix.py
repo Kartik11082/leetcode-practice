e406b3a7b0125b87de010733e5bba740b27a7ca9
Kartik Karkera <49190983+Kartik11082@users.noreply.github.com>
2022-06-27 13:15:44 +0530

    Time: 643 ms (20.00%), Space: 14.9 MB (40.00%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
@@ -1,20 +1,11 @@
 class Solution:
-	def checkXMatrix(self, grid: List[List[int]]) -> bool:
-		l = len(grid)
-		diagonal, otherElements = True, True
-		for i in range(l):
-			for j in range(l):
-				# print(grid[i][j])
-				if (i == j or i + j == l - 1) and (grid[i][j] != 0):
-					continue
-				elif (i != j or i + j != l - 1) and grid[i][j] != 0:
-					# print("Other element and non zero\n")
-					otherElements = False
-					break
-				elif (i == j or i + j == l - 1) and grid[i][j] == 0:
-					# print("\nDiagonal and zero\n")
-					diagonal = False
-					break
-				elif (i != j or i + j != l - 1) and grid[i][j] == 0:
-					continue
-		return (otherElements and diagonal)
+    def checkXMatrix(self, grid: List[List[int]]) -> bool:
+        l = len(grid)
+        for i in range(l):
+            for j in range(l):
+                if (i == j or i + j == (l - 1)):
+                    if grid[i][j] == 0:
+                        return False
+                elif grid[i][j] != 0:
+                    return False
+        return True
