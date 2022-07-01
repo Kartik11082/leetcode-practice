f91efe879091667a68d4723e9afccf0411c1dcc6
Kartik Karkera <49190983+Kartik11082@users.noreply.github.com>
2022-07-01 23:54:37 +0530

    Time: 1174 ms (35.32%), Space: 27.4 MB (54.60%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        localMaxArea, globalMaxArea = 0, 0
+        left, right = 0, len(height) - 1
+        
+        while left < right:
+            localMaxArea = min(height[left], height[right]) * (right - left)
+            if height[left] <= height[right]:
+                left += 1
+            else:
+                right -= 1
+            globalMaxArea = max(localMaxArea, globalMaxArea)
+        return globalMaxArea
