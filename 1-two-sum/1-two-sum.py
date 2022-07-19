f77e9b75bccd70789ff7ca21a8484cee138f043b
Kartik Karkera <49190983+Kartik11082@users.noreply.github.com>
2022-07-20 00:13:10 +0530

    Time: 40 ms (97.55%), Space: 14.3 MB (68.01%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -6,8 +6,20 @@ class Solution(object):
         :rtype: List[int]
         """
 
-        diffDict = dict()
-        for i, n in enumerate(nums):
-            if (target - n) in diffDict:
-                return [i, diffDict[target - n]]
-            diffDict[n] = i
+        # diffDict = dict()
+        # for i, n in enumerate(nums):
+        #     if (target - n) in diffDict:
+        #         return [i, diffDict[target - n]]
+        #     diffDict[n] = i
+        
+        d = {}
+        # Loop through the list
+        for i in range(len(nums)):
+            # Check if the difference is in the dictionary
+            if target - nums[i] in d:
+                # Return the index of the value
+                return [d[target - nums[i]], i]
+            # Add the value to the dictionary
+            d[nums[i]] = i
+        # Return None if the value is not in the dictionary
+        return None
