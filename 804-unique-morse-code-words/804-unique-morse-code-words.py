7ef494024efd587cdc0d23f86f92c60c3c971bb4
Kartik Karkera <49190983+Kartik11082@users.noreply.github.com>
2022-08-17 12:31:17 +0530

    Time: 60 ms (39.94%), Space: 13.9 MB (24.27%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.py
+++ 804-unique-morse-code-words/804-unique-morse-code-words.py
@@ -1,11 +1,8 @@
-class Solution:
-    def uniqueMorseRepresentations(self, words: List[str]) -> int:
-        morseCodes = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."];
-        res = [];
-        temp = "";
-        for word in words:
-            temp = "";
-            for char in word:
-                temp += morseCodes[ord(char) - 97]
-            res.append(temp)
-        return len(set(res))
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        MORSECODES = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+
+        seen = {"".join(MORSECODES[ord(c) - ord('a')] for c in word)
+                for word in words}
+
+        return len(seen)
