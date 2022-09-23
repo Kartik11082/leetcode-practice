2ff28ddd78fee11dc2bb4406514b1c78c69c00e8
Kartik Karkera <49190983+Kartik11082@users.noreply.github.com>
2022-09-23 11:10:04 +0530

    Time: 61 ms (34.20%), Space: 13.9 MB (7.58%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -1,10 +1,8 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        # convert int to binary string
-        data = bin(n)
-        # 0b000........ we are only selecting our string after 2nd index
-        data = data[2:]
-         # zfill is used to add zeros at the begining of the string until it reaches the length as specidifed
+        # convert int to binary string and remove '0b'
+        data = bin(n)[2:]
+         # zfill is used to add zeros at the begining of the string until it reaches the length as specified
         data = data.zfill(32)
         # slicing technique to reverse the string
         data = data[::-1]
